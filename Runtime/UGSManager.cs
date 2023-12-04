78e00df57216b2c6faa11c8bcd7a0a38a847c129
Ahmet Mertcan Dişli <1200112721@erciyes.edu.tr>
2023-12-04 16:41:47 +0300

    Update UGSManager.cs

--- Runtime/UGSManager.cs
+++ Runtime/UGSManager.cs
@@ -55,8 +55,8 @@ namespace LobbySystem.Scripts
                 // When using a ParrelSync clone, switch to a different authentication profile to force the clone
                 // to sign in as a different anonymous user account.
                 string customArgument = ClonesManager.GetArgument();
+                customArgument += Random.Range(0, 999).ToString();
                 AuthenticationService.Instance.SwitchProfile($"Clone_{customArgument}_Profile");
-                
                 Debug.Log("Clone detected, profile switched");
             }
 #endif
